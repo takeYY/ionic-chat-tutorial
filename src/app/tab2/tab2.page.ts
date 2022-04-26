aa4dcdca0fe7e5ea521f273dd1b681ee33b6dcc3
RyoYasutake <ryasutake@landscape.co.jp>
2022-04-26 16:24:04 +0900

    ✨ (tab2): プロフィール表示

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { ModalController } from '@ionic/angular';
 import { AuthService } from '../auth/auth.service';
+import { ProfilePage } from '../shared/profile/profile.page';
 
 @Component({
   selector: 'app-tab2',
@@ -7,7 +9,17 @@ import { AuthService } from '../auth/auth.service';
   styleUrls: ['tab2.page.scss'],
 })
 export class Tab2Page {
-  constructor(public auth: AuthService) {}
+  constructor(
+    public auth: AuthService,
+    public modalController: ModalController
+  ) {}
+
+  async openProfile() {
+    const modal = await this.modalController.create({
+      component: ProfilePage,
+    });
+    modal.present();
+  }
 
   signOut() {
     this.auth.authSignOut();
